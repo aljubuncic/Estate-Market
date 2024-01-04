91b744d3e6c47c92658b1c6578088dbf68ba28b3
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-01-04 18:25:53 +0100

    modify index,js

--- index.js
+++ index.js
@@ -12,87 +12,87 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(session({
-   secret: 'neka tajna sifra',
-   resave: true,
-   saveUninitialized: true
+    secret: 'neka tajna sifra',
+    resave: false,
+    saveUninitialized: true
 }));
 
-app.get('/detalji.html',function(req,res,next){
-    res.sendFile(__dirname + '/public/html/detalji.html',function(err){
-        if(err)
+app.get('/detalji.html', function (req, res, next) {
+    res.sendFile(__dirname + '/public/html/detalji.html', function (err) {
+        if (err)
             throw err;
     });
 });
 
-app.get('/meni.html',function(req,res,next){
-    res.sendFile(__dirname + '/public/html/meni.html',function(err){
-        if(err)
+app.get('/meni.html', function (req, res, next) {
+    res.sendFile(__dirname + '/public/html/meni.html', function (err) {
+        if (err)
             throw err;
     });
 });
 
-app.get('/nekretnine.html',function(req,res,next){
-    res.sendFile(__dirname + '/public/html/nekretnine.html',function(err){
-        if(err)
+app.get('/nekretnine.html', function (req, res, next) {
+    res.sendFile(__dirname + '/public/html/nekretnine.html', function (err) {
+        if (err)
             throw err;
     });
 });
 
-app.get('/prijava.html',function(req,res,next){
-    res.sendFile(__dirname + '/public/html/prijava.html',function(err){
-        if(err)
+app.get('/prijava.html', function (req, res, next) {
+    res.sendFile(__dirname + '/public/html/prijava.html', function (err) {
+        if (err)
             throw err;
     });
 });
 
-app.get('/profil.html',function(req,res,next){
-    res.sendFile(__dirname + '/public/html/profil.html',function(err){
-        if(err)
+app.get('/profil.html', function (req, res, next) {
+    res.sendFile(__dirname + '/public/html/profil.html', function (err) {
+        if (err)
             throw err;
     });
 });
 
-app.post('/login', function(req,res,next){
-    res.setHeader('Content-Type','application/json');
+app.post('/login', function (req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
     let requestBody = req.body;
-    fs.readFile(__dirname + '/data/korisnici.json','utf8',async function(err,data){
-        if(err)
+    fs.readFile(__dirname + '/data/korisnici.json', 'utf8', async function (err, data) {
+        if (err)
             throw err;
         let users = JSON.parse(data);
         let user = users.find(user => user.username == requestBody.username);
-        if(!user){
-            res.status(401).json({"greska": "Nepostojeći korisnik"});
+        if (!user) {
+            res.status(401).json({ "greska": "Nepostojeći korisnik" });
             return;
-        }     
-        bcrypt.compare(requestBody.password, user.password, function(err, result) {
-            if(result) {   
+        }
+        bcrypt.compare(requestBody.password, user.password, function (err, result) {
+            if (result) {
                 req.session.username = requestBody.username;
-                res.status(200).json({"poruka": "Uspješna prijava"});
+                res.status(200).json({ "poruka": "Uspješna prijava" });
             }
             else
-                res.status(401).json({"greska": "Neuspješna prijava"});
+                res.status(401).json({ "greska": "Neuspješna prijava" });
         });
     });
 });
 
-app.post('/logout',function(req,res,next){
-    res.setHeader('Content-Type','application/json');
-    if(req.session.username){
+app.post('/logout', function (req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    if (req.session.username) {
         req.session.username = null;
-        res.status(200).json({"poruka":"Uspješno ste se odjavili"});
+        res.status(200).json({ "poruka": "Uspješno ste se odjavili" });
     }
     else
-        res.status(401).json({"greska":"Neautorizovan pristup"});
+        res.status(401).json({ "greska": "Neautorizovan pristup" });
 });
 
-app.get('/korisnik',function(req,res,next){
-    res.setHeader('Content-Type','application/json');
-    if(!req.session.username){
-        res.status(401).json({"greska":"Neautorizovan pristup"});
+app.get('/korisnik', function (req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    if (!req.session.username) {
+        res.status(401).json({ "greska": "Neautorizovan pristup" });
         return;
     }
-    fs.readFile(__dirname + '/data/korisnici.json','utf8',function(err,data){
-        if(err)
+    fs.readFile(__dirname + '/data/korisnici.json', 'utf8', function (err, data) {
+        if (err)
             throw err;
         let users = JSON.parse(data);
         let user = users.find(u => u.username == req.session.username);
@@ -100,24 +100,24 @@ app.get('/korisnik',function(req,res,next){
     });
 });
 
-app.post('/upit',function(req,res,next){
-    res.setHeader('Content-Type','application/json');
-    if(!req.session.username){
-        res.status(401).json({"greska":"Neautorizovan pristup"});
+app.post('/upit', function (req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    if (!req.session.username) {
+        res.status(401).json({ "greska": "Neautorizovan pristup" });
         return;
     }
     let requestBody = req.body;
-    fs.readFile(__dirname + '/data/nekretnine.json','utf8',function(err,data){
-        if(err)
+    fs.readFile(__dirname + '/data/nekretnine.json', 'utf8', function (err, data) {
+        if (err)
             throw err;
         let estates = JSON.parse(data);
         let estate = estates.find(e => e.id == requestBody.nekretnina_id);
-        if(!estate){
-            res.status(400).json({"greska":`Nekretnina sa id-em ${requestBody.nekretnina_id} ne postoji`});
+        if (!estate) {
+            res.status(400).json({ "greska": `Nekretnina sa id-em ${requestBody.nekretnina_id} ne postoji` });
             return;
         }
-        fs.readFile(__dirname + '/data/korisnici.json','utf8',async function(err,data){
-            if(err)
+        fs.readFile(__dirname + '/data/korisnici.json', 'utf8', async function (err, data) {
+            if (err)
                 throw err;
             let users = JSON.parse(data);
             let user = users.find(u => u.username == req.session.username);
@@ -125,163 +125,175 @@ app.post('/upit',function(req,res,next){
                 korisnik_id: user.id,
                 tekst_upita: requestBody.tekst_upita
             });
-            await updateEntityInJsonFile(estates,estate,'nekretnine',res,'Upit je uspješno dodan');   
+            await updateEntityInJsonFile(estates, estate, 'nekretnine', res, 'Upit je uspješno dodan');
         });
     });
 });
 
-app.put('/korisnik',function(req,res,next){
-    res.setHeader('Content-Type','application/json');
-    if(!req.session.username){
-        res.status(401).json({"greska":"Neautorizovan pristup"});
+app.put('/korisnik', function (req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    if (!req.session.username) {
+        res.status(401).json({ "greska": "Neautorizovan pristup" });
         return;
     }
     let requestBody = req.body;
-    fs.readFile(__dirname + '/data/korisnici.json','utf8',async function(err,data){
-        if(err)
+    fs.readFile(__dirname + '/data/korisnici.json', 'utf8', async function (err, data) {
+        if (err)
             throw err;
         let users = JSON.parse(data);
         let user = users.find(u => u.username == req.session.username);
 
-        if('password' in requestBody){
-            bcrypt.hash(requestBody.password, 10, async function(err, hash) {
+        if ('password' in requestBody) {
+            bcrypt.hash(requestBody.password, 10, async function (err, hash) {
                 user.password = hash;
-                for(let key in requestBody){
-                    if(key != 'password')  
+                for (let key in requestBody) {
+                    if (key != 'password')
                         user[key] = requestBody[key];
                 }
-                await updateEntityInJsonFile(users,user,'korisnici',res,'Podaci su uspješno ažurirani');   
+                await updateEntityInJsonFile(users, user, 'korisnici', res, 'Podaci su uspješno ažurirani');
             });
         }
         else {
-            for(let key in requestBody){
+            for (let key in requestBody) {
                 user[key] = requestBody[key];
             }
-            await updateEntityInJsonFile(users,user,'korisnici',res,'Podaci su uspješno ažurirani');
-        }   
+            await updateEntityInJsonFile(users, user, 'korisnici', res, 'Podaci su uspješno ažurirani');
+        }
     });
 });
 
-app.get('/nekretnine', function(req,res,next){
-    res.setHeader('Content-Type','application/json');
-    fs.readFile(__dirname + '/data/nekretnine.json','utf8',function(err,data){
-        if(err)
+app.get('/nekretnine', function (req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    fs.readFile(__dirname + '/data/nekretnine.json', 'utf8', function (err, data) {
+        if (err)
             throw err;
         let estates = JSON.parse(data);
         res.status(200).json(estates);
     });
 });
 
-app.post('/marketing/nekretnine', function(req,res,next){
-    res.setHeader('Content-Type','application/json');
-    requestBody = req.body;
-    nekretnine_ids = requestBody.nizNekretnina;
-    fs.readFile(__dirname + '/data/klikovipretrage.json','utf8',async function(err,data){
-        if(err)
+app.post('/marketing/nekretnine', function (req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    let requestBody = req.body;
+    let nekretnineIds = requestBody.nizNekretnina;
+    fs.readFile(__dirname + '/data/klikovipretrage.json', 'utf8', function (err, data) {
+        if (err)
             throw err;
         klikovipretrage = JSON.parse(data);
-        for(nekretnina_id of nekretnine_ids){
-            klikpretraga = klikovipretrage.find(x => x.id == nekretnina_id);
-            if(klikpretraga){
-                object = {
-                    id: nekretnina_id,
-                    pretrage: klikpretraga.pretrage + 1,
-                    klikovi: klikpretraga.klikovi
-                }
-                klikovipretrage = await updateEntityInJsonFile(klikovipretrage,object,'klikovipretrage',null,null); 
-            }
-            else {
-                object = {
-                    id: nekretnina_id,
+        for(let nekretninaId of nekretnineIds){
+            let index = klikovipretrage.findIndex(x => x.id == nekretninaId);
+            if(index == -1)
+                klikovipretrage.push({
+                    id: nekretninaId,
                     pretrage: 1,
                     klikovi: 0
-                }
-                klikovipretrage = await updateEntityInJsonFile(klikovipretrage,object,'klikovipretrage',null,null);
-            }
+                });
+            else
+                klikovipretrage[index].pretrage++;
         }
-        res.status(200).end();
+        fs.writeFile(__dirname + '/data/klikovipretrage.json',JSON.stringify(klikovipretrage),'utf8',function(error){
+            if(error)
+                throw error;
+            res.status(200).end();
+        });
     });
 });
 
-app.post('/marketing/nekretnina/:id',function(req,res,next){
-    res.setHeader('Content-Type','application/json');
+app.post('/marketing/nekretnina/:id', function (req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
     nekretnina_id = parseInt(req.params.id);
-    fs.readFile(__dirname + '/data/klikovipretrage.json',async function(err,data){
-        if(err)
-            throw err;  
+    fs.readFile(__dirname + '/data/klikovipretrage.json', 'utf8',function (err, data) {
+        if (err)
+            throw err;
         klikovipretrage = JSON.parse(data);
-        klikpretraga = klikovipretrage.find(x => x.id == nekretnina_id);
-        if(!klikpretraga)
-            object = {
+        index = klikovipretrage.findIndex(x => x.id == nekretnina_id);
+        if (index == -1)
+            klikovipretrage.push({
                 id: nekretnina_id,
                 pretrage: 0,
                 klikovi: 1
-            }
+            });
         else
-            object = {
-                id: nekretnina_id,
-                pretrage: klikpretraga.pretrage,
-                klikovi: klikpretraga.klikovi + 1
-            }
-            
-        await updateEntityInJsonFile(klikovipretrage,object,'klikovipretrage',res,null);
-    })
+            klikovipretrage[index].klikovi++;
+        fs.writeFile(__dirname + '/data/klikovipretrage.json',JSON.stringify(klikovipretrage),'utf8',function(error){
+            if(error)
+                throw error;
+            res.status(200).end();
+        });
+    });
 });
 
-app.post('/marketing/osvjezi',async function(req,res,next){
-    res.setHeader('Content-Type','application/json');
-    try{
-        let nekretnineIds = [];
-        if(req.body.nizNekretnina){
-            nekretnineIds = req.body.nizNekretnina;
-            req.session.nekretnineIds = nekretnineIds;
-        }
-        else{
-            nekretnineIds = req.session.nekretnineIds;
-        }
-        let data = await fs.promises.readFile(__dirname + '/data/klikovipretrage.json','utf8');
-        klikovipretrage = JSON.parse(data);
+app.post('/marketing/osvjezi', function (req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+
+    let nekretnineIds = [];
+    console.log('--------------------------');
+    console.log('Request body: ', req.body);
+    if (req.body.nizNekretnina) {
+        nekretnineIds = req.body.nizNekretnina;
+        req.session.nekretnineIds = nekretnineIds;
+        console.log('req.session.nekretnineIds: (kada je req.body neprazan) ',req.session.nekretnineIds)
+    }
+    else {
+        nekretnineIds = req.session.nekretnineIds;
+    }
+    console.log('req.session.nekretnineIds: ', req.session.nekretnineIds);
+    console.log('Trenutne NekretnineIds: ', nekretnineIds);
+    console.log('Prethodni odgovor nekretnine: ', req.session.prethodniOdgovorNekretnine);
+    
+    fs.readFile(__dirname + '/data/klikovipretrage.json', 'utf8', function (error, data) {
+        if(error)
+            throw error;
+        let klikovipretrage = JSON.parse(data);
         klikovipretrage = klikovipretrage.filter(x => nekretnineIds.find(y => y == x.id));
+        //provjera da li postoje neke nekretnine bez pretraga i klikova
+        for(nekretninaId of nekretnineIds){
+            if(!klikovipretrage.find(x => x.id == nekretninaId))
+                klikovipretrage.push({
+                    id: nekretninaId,
+                    pretrage: 0,
+                    klikovi: 0
+                });
+        }
         //filtirianje na osnovu da li se promjenio broj klikova i pretraga za pojedine nekretnine u odnosu na prethodni poziv 
-        result = klikovipretrage.filter(function(element){
-            let prethodniPozivNekretnina = null;
-            if(req.session.prethodniPozivNekretnine) // provjera da li je bilo poziva od prije
-                prethodniPozivNekretnina = req.session.prethodniPozivNekretnine.find(x => x.id == element.id);
-            if(!prethodniPozivNekretnina) // ako je neka nova nekeretnina koje nije bilo u proslom pozivu
+        klikovipretrageSaPromjenom = klikovipretrage.filter(function (element) {
+            let prethodniOdgovorNekretnina = null;
+            if (req.session.prethodniOdgovorNekretnine) // provjera da li je bilo poziva od prije
+                prethodniOdgovorNekretnina = req.session.prethodniOdgovorNekretnine.find(x => x.id == element.id);
+            if (!prethodniOdgovorNekretnina) // ako je neka nova nekeretnina koje nije bilo u proslom pozivu
                 return true;
             //provjera da li su se vrijednosti klikova i pretraga promjenile
-            if(prethodniPozivNekretnina.pretrage != element.pretrage || prethodniPozivNekretnina.klikovi != element.klikovi)
+            if (prethodniOdgovorNekretnina.pretrage != element.pretrage || prethodniOdgovorNekretnina.klikovi != element.klikovi)
                 return true;
             else
                 return false;
         });
 
-        req.session.prethodniPozivNekretnine = klikovipretrage;
-            res.status(200).json({
-                nizNekretnina: result
-            });
-        }
-    catch(ex){
-        throw ex;
-    }
-});
+        req.session.prethodniOdgovorNekretnine = klikovipretrage;
+        console.log('Vraceni podaci: ', klikovipretrageSaPromjenom);
+        res.status(200).json({
+            nizNekretnina: klikovipretrageSaPromjenom
+        });
 
+    });
+});
 
 app.listen(port);
 
-
-async function updateEntityInJsonFile(entities, entity, JSONFile, res, responseMessage){
-    entities = entities.filter(e => e.id!=entity.id);
-    entities.push(entity);
-    try{
-        await fs.promises.writeFile(__dirname + `/data/${JSONFile}.json`,JSON.stringify(entities));
+async function updateEntityInJsonFile(entities, entity, JSONFile, res, responseMessage) {
+    if(entity){
+        entities = entities.filter(e => e.id != entity.id);
+        entities.push(entity);
     }
-    catch(err){
+    try {
+        await fs.promises.writeFile(__dirname + `/data/${JSONFile}.json`, JSON.stringify(entities));
+    }
+    catch (err) {
         throw err;
     }
-    if(res){
-        if(responseMessage)
-            res.status(200).send({"poruka": responseMessage});
+    if (res) {
+        if (responseMessage)
+            res.status(200).send({ "poruka": responseMessage });
         else
             res.status(200).end();
     }
